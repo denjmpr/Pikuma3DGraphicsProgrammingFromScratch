65baaaa96e7b0c343549ad848ce65bca98ac2810
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-06-27 10:46:40 +0500

    Completed "Coding New Header Files".

--- display.c
+++ display.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <SDL.h>
-
-bool is_running = false;
+#include  <stdio.h>
+#include "display.h"
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-
 uint32_t* color_buffer = NULL;
 SDL_Texture* color_buffer_texture = NULL;
-
 int window_width = 800;
 int window_height = 600;
 
@@ -49,37 +43,6 @@ bool initialize_window(void) {
 	return true;
 }
 
-void setup(void) {
-	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
-
-	color_buffer_texture = SDL_CreateTexture(
-		renderer,
-		SDL_PIXELFORMAT_ARGB8888,
-		SDL_TEXTUREACCESS_STREAMING,
-		window_width,
-		window_height
-	);
-}
-
-void process_input(void) {
-	SDL_Event event;
-	SDL_PollEvent(&event);
-
-	switch (event.type) {
-	case SDL_QUIT:
-		is_running = false;
-		break;
-	case SDL_KEYDOWN:
-		if (event.key.keysym.sym == SDLK_ESCAPE)
-			is_running = false;
-		break;
-	}
-}
-
-void update(void) {
-
-}
-
 void draw_grid(void) {
 	for (int y = 0; y < window_height; y += 10) {
 		for (int x = 0; x < window_width; x += 10) {
@@ -116,40 +79,9 @@ void clear_color_buffer(uint32_t color) {
 	}
 }
 
-void render(void) {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	SDL_RenderClear(renderer);
-
-	draw_grid();
-
-	draw_rect(300, 200, 300, 150, 0xFFFF00FF);
-
-	render_color_buffer();
-	clear_color_buffer(0xFF000000);
-
-	SDL_RenderPresent(renderer);
-}
-
 void destroy_window(void) {
 	free(color_buffer);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-}
-
-int main(int argc, char* args[]) {
-
-	is_running = initialize_window();
-
-	setup();
-
-	while (is_running) {
-		process_input();
-		update();
-		render();
-	}
-
-	destroy_window();
-
-	return 0;
 }
