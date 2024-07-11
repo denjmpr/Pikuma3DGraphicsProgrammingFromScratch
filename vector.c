21291ba3682b06814fd42982b10e2e020dfebd6f
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-11 12:02:02 +0500

    Completed "Dot Product".

--- vector.c
+++ vector.c
@@ -37,6 +37,10 @@ vec2_t vec2_div(vec2_t v, float factor) {
 	return result;
 }
 
+float vec2_dot(vec2_t a, vec2_t b) {
+	return (a.x * b.x) + (a.y * b.y);
+}
+
 float vec3_length(vec3_t v) {
 	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
@@ -84,6 +88,10 @@ vec3_t vec3_cross(vec3_t a, vec3_t b) {
 		.z = a.x * b.y - a.y * b.x
 	};
 	return result;
+}
+
+float vec3_dot(vec3_t a, vec3_t b) {
+	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 }
 
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
