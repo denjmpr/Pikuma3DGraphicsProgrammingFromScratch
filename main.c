91576073850e808edf4930d060f5ef20e3387548
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-15 17:15:43 +0500

    Completed "Exercise: Selecting Different Render Modes".

--- main.c
+++ main.c
@@ -16,6 +16,9 @@ bool is_running = false;
 int previous_frame_time = 0;
 
 void setup(void) {
+	render_method = RENDER_WIRE;
+	cull_method = CULL_BACKFACE;
+
 	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
 
 	color_buffer_texture = SDL_CreateTexture(
@@ -41,6 +44,18 @@ void process_input(void) {
 	case SDL_KEYDOWN:
 		if (event.key.keysym.sym == SDLK_ESCAPE)
 			is_running = false;
+		if (event.key.keysym.sym == SDLK_1)
+			render_method = RENDER_WIRE_VERTEX;
+		if (event.key.keysym.sym == SDLK_2)
+			render_method = RENDER_WIRE;
+		if (event.key.keysym.sym == SDLK_3)
+			render_method = RENDER_FILL_TRIANGLE;
+		if (event.key.keysym.sym == SDLK_4)
+			render_method = RENDER_FILL_TRIANGLE_WIRE;
+		if (event.key.keysym.sym == SDLK_c)
+			cull_method = CULL_BACKFACE;
+		if (event.key.keysym.sym == SDLK_d)
+			cull_method = CULL_NONE;
 		break;
 	}
 }
@@ -80,7 +95,7 @@ void update(void) {
 		triangle_t projected_triangle;
 
 		vec3_t transformed_vetices[3];
-		
+
 		for (int j = 0; j < 3; j++) {
 			vec3_t transformed_vertex = face_vertices[j];
 			transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
@@ -92,24 +107,26 @@ void update(void) {
 			transformed_vetices[j] = transformed_vertex;
 		}
 
-		vec3_t vector_a = transformed_vetices[0];
-		vec3_t vector_b = transformed_vetices[1];
-		vec3_t vector_c = transformed_vetices[2];
+		if (cull_method == CULL_BACKFACE) {
+			vec3_t vector_a = transformed_vetices[0];
+			vec3_t vector_b = transformed_vetices[1];
+			vec3_t vector_c = transformed_vetices[2];
 
-		vec3_t vector_ab = vec3_sub(vector_b, vector_a);
-		vec3_t vector_ac = vec3_sub(vector_c, vector_a);
-		vec3_normalize(&vector_ab);
-		vec3_normalize(&vector_ac);
+			vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+			vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+			vec3_normalize(&vector_ab);
+			vec3_normalize(&vector_ac);
 
-		vec3_t normal = vec3_cross(vector_ab, vector_ac);
-		vec3_normalize(&normal);
+			vec3_t normal = vec3_cross(vector_ab, vector_ac);
+			vec3_normalize(&normal);
 
-		vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+			vec3_t camera_ray = vec3_sub(camera_position, vector_a);
 
-		float dot_normal_camera = vec3_dot(normal, camera_ray);
+			float dot_normal_camera = vec3_dot(normal, camera_ray);
 
-		if (dot_normal_camera < 0) {
-			continue;
+			if (dot_normal_camera < 0) {
+				continue;
+			}
 		}
 
 		for (int j = 0; j < 3; j++) {
@@ -132,19 +149,29 @@ void render(void) {
 	for (int i = 0; i < num_triangles; i++) {
 		triangle_t triangle = triangles_to_render[i];
 
-		draw_filled_triangle(
-			triangle.points[0].x, triangle.points[0].y,
-			triangle.points[1].x, triangle.points[1].y,
-			triangle.points[2].x, triangle.points[2].y,
-			0xFFFFFFFF
-		);
+		if (render_method == RENDER_FILL_TRIANGLE || render_method == RENDER_FILL_TRIANGLE_WIRE) {
+			draw_filled_triangle(
+				triangle.points[0].x, triangle.points[0].y,
+				triangle.points[1].x, triangle.points[1].y,
+				triangle.points[2].x, triangle.points[2].y,
+				0xFF555555
+			);
+		}
 
-		draw_triangle(
-			triangle.points[0].x, triangle.points[0].y,
-			triangle.points[1].x, triangle.points[1].y,
-			triangle.points[2].x, triangle.points[2].y,
-			0xFF000000
-		);
+		if (render_method == RENDER_WIRE || render_method == RENDER_WIRE_VERTEX || render_method == RENDER_FILL_TRIANGLE_WIRE) {
+			draw_triangle(
+				triangle.points[0].x, triangle.points[0].y,
+				triangle.points[1].x, triangle.points[1].y,
+				triangle.points[2].x, triangle.points[2].y,
+				0xFFFFFFFF
+			);
+		}
+
+		if (render_method == RENDER_WIRE_VERTEX) {
+			draw_rect(triangle.points[0].x - 3, triangle.points[0].y - 3, 6, 6, 0xFFFF0000);
+			draw_rect(triangle.points[1].x - 3, triangle.points[1].y - 3, 6, 6, 0xFFFF0000);
+			draw_rect(triangle.points[2].x - 3, triangle.points[2].y - 3, 6, 6, 0xFFFF0000);
+		}
 	}
 
 	array_free(triangles_to_render);
