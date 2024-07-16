cd2ff4cd62e731af0723ac15ca64c2ea805285a8
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-16 14:41:31 +0500

    Completed "Exercise: Sorting Faces".

--- main.c
+++ main.c
@@ -149,6 +149,17 @@ void update(void) {
 		};
 
 		array_push(triangles_to_render, projected_triangle);
+
+		int num_triangles = array_length(triangles_to_render);
+		for (int i = 0; i < num_triangles; i++) {
+			for (int j = i; j < num_triangles; j++) {
+				if (triangles_to_render[i].avg_depth < triangles_to_render[j].avg_depth) {
+					triangle_t temp = triangles_to_render[i];
+					triangles_to_render[i] = triangles_to_render[j];
+					triangles_to_render[j] = temp;
+				}
+			}
+		}
 	}
 }
 
