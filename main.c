e7d38747199b90c1bf0ae7c92b6c28c2e91d8287
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-19 16:37:40 +0500

    Completed "Translation Matrix Code".

--- main.c
+++ main.c
@@ -85,8 +85,11 @@ void update(void) {
 	mesh.rotation.z += 0.01;
 	mesh.scale.x += 0.002;
 	mesh.scale.y += 0.001;
+	mesh.translation.x += 0.01;
+	mesh.translation.z = 5.0;
 
 	mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
+	mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
 
 	int num_faces = array_length(mesh.faces);
 	for (int i = 0; i < num_faces; i++) {
@@ -103,8 +106,7 @@ void update(void) {
 			vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
 			transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-
-			transformed_vertex.z += 5;
+			transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
 			transformed_vetices[j] = transformed_vertex;
 		}
