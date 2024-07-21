4bdca9960cacfae6f2d424737098e693ee21455d
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-21 13:25:29 +0500

    Completed "Order of Transformations".

--- main.c
+++ main.c
@@ -109,6 +109,7 @@ void update(void) {
 			vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
 			mat4_t world_matrix = mat4_identity();
+
 			world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
 			world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
 			world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
