ef1ad7f4703238fcf08bbf63d8aa406bd09eba3b
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-25 11:55:35 +0500

    Completed "Loading OBJ Texture Attributes".

--- mesh.c
+++ mesh.c
@@ -52,6 +52,8 @@ void load_obj_file_data(char* filename) {
 	FILE* file;
 	fopen_s(&file, filename, "r");
 
+	tex2_t* texcoords = NULL;
+
 	char line[1024];
 	while (fgets(line, 1024, file)) {
 		if (strncmp(line, "v ", 2) == 0) {
@@ -59,6 +61,13 @@ void load_obj_file_data(char* filename) {
 			sscanf_s(line, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
 			array_push(mesh.vertices, vertex);
 		}
+
+		if (strncmp(line, "vt ", 3) == 0) {
+			tex2_t texcoord;
+			sscanf_s(line, "vt %f %f", &texcoord.u, &texcoord.v);
+			array_push(texcoords, texcoord);
+		}
+
 		if (strncmp(line, "f ", 2) == 0) {
 			int vertex_indices[3];
 			int texture_indices[3];
@@ -69,12 +78,16 @@ void load_obj_file_data(char* filename) {
 				&vertex_indices[2], &texture_indices[2], &normal_indices[2]
 			);
 			face_t face = {
-				.a = vertex_indices[0],
-				.b = vertex_indices[1],
-				.c = vertex_indices[2],
+				.a = vertex_indices[0] - 1,
+				.b = vertex_indices[1] - 1,
+				.c = vertex_indices[2] - 1,
+				.a_uv = texcoords[texture_indices[0] - 1],
+				.b_uv = texcoords[texture_indices[1] - 1],
+				.c_uv = texcoords[texture_indices[2] - 1],
 				.color = 0xFFFFFFFF
 			};
 			array_push(mesh.faces, face);
 		}
 	}
+	array_free(texcoords);
 }
--- triangle.c
+++ triangle.c
@@ -148,6 +148,10 @@ void draw_textured_triangle(
 		float_swap(&v0, &v1);
 	}
 
+	v0 = 1.0 - v0;
+	v1 = 1.0 - v1;
+	v2 = 1.0 - v2;
+
 	vec4_t point_a = { x0, y0, z0, w0 };
 	vec4_t point_b = { x1, y1, z1, w1 };
 	vec4_t point_c = { x2, y2, z2, w2 };
