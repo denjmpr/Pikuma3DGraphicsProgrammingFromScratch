ea3b3defd64cb2a69bf8a6a0ad01cf96d5996e35
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-07-31 13:53:44 +0500

    Completed "Coding the Function to Clip Polygons Against Planes".

--- clipping.c
+++ clipping.c
@@ -48,7 +48,48 @@ polygon_t create_polygon_from_triangle(vec3_t v0, vec3_t v1, vec3_t v2) {
 }
 
 void clip_polygon_against_plane(polygon_t* polygon, int plane) {
+	vec3_t plane_point = frustum_planes[plane].point;
+	vec3_t plane_normal = frustum_planes[plane].normal;
 
+	vec3_t inside_vertices[MAX_NUM_POLY_VERTICES];
+	int num_inside_vertices = 0;
+
+	vec3_t* current_vertex = &polygon->vertices[0];
+	vec3_t* previous_vertex = &polygon->vertices[polygon->num_vertices - 1];
+
+	float current_dot = 0;
+	float previous_dot = vec3_dot(vec3_sub(*previous_vertex, plane_point), plane_normal);
+
+	while (current_vertex != &polygon->vertices[polygon->num_vertices]) {
+		current_dot = vec3_dot(vec3_sub(*current_vertex, plane_point), plane_normal);
+
+		if (current_dot * previous_dot < 0) {
+			float t = previous_dot / (previous_dot - current_dot);
+
+			vec3_t intersection_point = vec3_clone(current_vertex);
+			intersection_point = vec3_sub(intersection_point, *previous_vertex);
+			intersection_point = vec3_mul(intersection_point, t);
+			intersection_point = vec3_add(intersection_point , *previous_vertex);
+
+			inside_vertices[num_inside_vertices] = vec3_clone(&intersection_point);
+			num_inside_vertices++;
+		}
+
+		if (current_dot > 0) {
+			inside_vertices[num_inside_vertices] = vec3_clone(current_vertex);
+			num_inside_vertices++;
+		}
+
+		previous_dot = current_dot;
+		previous_vertex = current_vertex;
+		current_vertex++;
+	}
+
+	for (int i = 0; i < num_inside_vertices; i++) {
+		polygon->vertices[i] = vec3_clone(&inside_vertices[i]);
+	}
+
+	polygon->num_vertices = num_inside_vertices;
 }
 
 void clip_polygon(polygon_t* polygon) {
