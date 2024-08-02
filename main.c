b29859dcaffc61f4f0f33bfda32caf65dfc3e589
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-02 20:44:53 +0500

    Completed "Exercise: Camera Pitch Rotation".

--- main.c
+++ main.c
@@ -91,30 +91,30 @@ void process_input(void) {
 				set_cull_method(CULL_NONE);
 				break;
 			}
-			if (event.key.keysym.sym == SDLK_UP) {
-				camera.position.y += 3.0 * delta_time;
-				break;
-			}
-			if (event.key.keysym.sym == SDLK_DOWN) {
-				camera.position.y -= 3.0 * delta_time;
-				break;
-			}
-			if (event.key.keysym.sym == SDLK_a) {
-				camera.yaw -= 1.0 * delta_time;
-				break;
-			}
-			if (event.key.keysym.sym == SDLK_d) {
-				camera.yaw += 1.0 * delta_time;
-				break;
-			}
 			if (event.key.keysym.sym == SDLK_w) {
-				camera.forward_velocity = vec3_mul(camera.direction, 5.0 * delta_time);
-				camera.position = vec3_add(camera.position, camera.forward_velocity);
+				rotate_camera_pitch(+3.0 * delta_time);
 				break;
 			}
 			if (event.key.keysym.sym == SDLK_s) {
-				camera.forward_velocity = vec3_mul(camera.direction, 5.0 * delta_time);
-				camera.position = vec3_sub(camera.position, camera.forward_velocity);
+				rotate_camera_pitch(-3.0 * delta_time);
+				break;
+			}
+			if (event.key.keysym.sym == SDLK_RIGHT) {
+				rotate_camera_yaw(+1.0 * delta_time);
+				break;
+			}
+			if (event.key.keysym.sym == SDLK_LEFT) {
+				rotate_camera_yaw(-1.0 * delta_time);
+				break;
+			}
+			if (event.key.keysym.sym == SDLK_UP) {
+				update_camera_forward_velocity(vec3_mul(get_camera_direction(), 5.0 * delta_time));
+				update_camera_position(vec3_add(get_camera_position(), get_camera_forward_velocity()));
+				break;
+			}
+			if (event.key.keysym.sym == SDLK_DOWN) {
+				update_camera_forward_velocity(vec3_mul(get_camera_direction(), 5.0 * delta_time));
+				update_camera_position(vec3_sub(get_camera_position(), get_camera_forward_velocity()));
 				break;
 			}
 			break;
@@ -140,20 +140,15 @@ void update(void) {
 	mesh.rotation.z += 0.0 * delta_time;
 	mesh.translation.z = 5.0;
 
-	vec3_t target = { 0, 0, 1 };
-	mat4_t camera_yaw_rotation = mat4_make_rotation_y(camera.yaw);
-	camera.direction = vec3_from_vec4(mat4_mul_vec4(camera_yaw_rotation, vec4_from_vec3(target)));
-
-	target = vec3_add(camera.position, camera.direction);
-	vec3_t up_direction = { 0, 1, 0 };
-
-	view_matrix = mat4_look_at(camera.position, target, up_direction);
-
 	mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
 	mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
 	mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
 	mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
 	mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
+
+	vec3_t target = get_camera_lookat_target();
+	vec3_t up_direction = { 0, 1, 0 };
+	view_matrix = mat4_look_at(get_camera_position(), target, up_direction);
 
 	int num_faces = array_length(mesh.faces);
 	for (int i = 0; i < num_faces; i++) {
