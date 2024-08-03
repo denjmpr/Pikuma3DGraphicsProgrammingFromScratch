32d2d3287db254e85dc18d725d9cda15cef75a01
Denis Zmitrichenko <den.jmpr@gmail.com>
2024-08-03 21:24:29 +0500

    Completed "Finishing our Implementation".

--- triangle.c
+++ triangle.c
@@ -2,6 +2,22 @@
 #include "swap.h"
 #include "triangle.h"
 
+vec3_t get_triangle_normal(vec4_t vertices[3]) {
+	vec3_t vector_a = vec3_from_vec4(vertices[0]);
+	vec3_t vector_b = vec3_from_vec4(vertices[1]);
+	vec3_t vector_c = vec3_from_vec4(vertices[2]);
+
+	vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+	vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+	vec3_normalize(&vector_ab);
+	vec3_normalize(&vector_ac);
+
+	vec3_t normal = vec3_cross(vector_ab, vector_ac);
+	vec3_normalize(&normal);
+
+	return normal;
+}
+
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
 	draw_line(x0, y0, x1, y1, color);
 	draw_line(x1, y1, x2, y2, color);
